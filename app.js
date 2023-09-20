b6a382457b86b5137ef855fa4b1cd989c36021fb
Samaa-Hazem2001 <samaaabdellatif957@gmail.com>
2023-09-20 17:12:35 +0300

    phase2

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-// console.log("hello");
+//CORE modules
+//NOTE: we use "require" to import certain package
 const readline = require ('readline')
 const fs = require('fs')
+const url =  require('url')
 // const rl = readline.createInterface({
 //     input : process.stdin,
 //     output : process.stdout
@@ -40,23 +42,30 @@ const html = fs.readFileSync('./Template/index.html', 'utf-8')
 const http = require('http');
 let products = JSON.parse(fs.readFileSync('./Data/products.json', 'utf-8' ) )
 let productLisyHtml = fs.readFileSync('./Template/products-list.html', 'utf-8')
+let productDetailHtml = fs.readFileSync('./Template/product-details.html', 'utf-8')
 
-let productHtmlArray = products.map((prod) =>{
-    let output = productLisyHtml.replace('{{%IMAGE%}}',prod.productImage)
-    output = output.replace('{{%NAME%}}',prod.name)
-    output = output.replace('{{%MODELNAME%}}',prod.modeName)
-    output = output.replace('{{%MODELNO%}}',prod.modelNumber)
-    output = output.replace('{{%SIZE%}}',prod.size)
-    output = output.replace('{{%CAMERA%}}',prod.camera)
-    output = output.replace('{{%PRICE%}}',prod.price)
-    output = output.replace('{{%COLOR%}}',prod.color)
+function replaceHtml(template,product) {
+    let output = template.replace('{{%IMAGE%}}',product.productImage)
+    output = output.replace('{{%NAME%}}',product.name)
+    output = output.replace('{{%MODELNAME%}}',product.modeName)
+    output = output.replace('{{%MODELNO%}}',product.modelNumber)
+    output = output.replace('{{%SIZE%}}',product.size)
+    output = output.replace('{{%CAMERA%}}',product.camera)
+    output = output.replace('{{%PRICE%}}',product.price)
+    output = output.replace('{{%COLOR%}}',product.color)
+    output = output.replace('{{%ID%}}',product.id)
+    output = output.replace('{{%ROM%}}', product.ROM);
+    output = output.replace('{{%DESC%}}', product.Description);
 
     return output; //?
-})
-
+}
 //create a server:
 const server = http.createServer((request, response) => {
-    let path = request.url;
+    
+    // let x = url.parse(request.url, true) 
+    // console.log(x) // NOTE: pathname property store the source name' value
+    let {query , pathname: path} = url.parse(request.url, true) //? url.parse? true? what is this: "pathname : path" ?
+    // let path = request.url;
     //response.end(path)
     if(path === '/' || path.toLocaleLowerCase() === '/home' ){
         response.writeHead(200,{
@@ -77,12 +86,21 @@ const server = http.createServer((request, response) => {
         })
         response.end(html.replace('{{%CONTENT%}}','you are in contact page'))
     } else if (path.toLocaleLowerCase() === '/products' ){
-        let productResponseHtml = html.replace('{{%CONTENT%}}',productHtmlArray.join(','))
+        if(!query.id) {
+            let productHtmlArray = products.map((prod)=>{
+                return replaceHtml(productLisyHtml,prod)
+            })
+            let productResponseHtml = html.replace('{{%CONTENT%}}',productHtmlArray.join(','))
          response.writeHead(200,{
             'Content-Type' : 'text/html'
         });
-
         response.end(productResponseHtml)
+        } else {
+            let prod = products[query.id]
+            let productDetailResponceHtml = replaceHtml(productDetailHtml,prod)
+            response.end(html.replace('{{%CONTENT%}}',productDetailResponceHtml))
+        }
+
         //console.log(productHtmlArray);
         // fs.readFile('./Data/products.json', 'utf-8' , (error, data) => {
         //     let products = JSON.parse(data) // 
